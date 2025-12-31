5505baa38730c89d8e4c31aaf555d95f60cfd8cf
Test User <test@test.com>
2025-12-31 10:52:13 -0500

    Fix sim_main.cpp: add timeInc for Verilator timing mode

--- verif/sim_main.cpp
+++ verif/sim_main.cpp
@@ -2,12 +2,18 @@
 #include "Vaxi4_top_tb.h"
 
 int main(int argc, char** argv) {
-    Verilated::commandArgs(argc, argv);
-    Vaxi4_top_tb* top = new Vaxi4_top_tb;
-    while (!Verilated::gotFinish()) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    
+    Vaxi4_top_tb* top = new Vaxi4_top_tb{contextp};
+    
+    while (!contextp->gotFinish()) {
+        contextp->timeInc(1);
         top->eval();
     }
+    
     top->final();
     delete top;
+    delete contextp;
     return 0;
 }
